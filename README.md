c2d22242b70d51991e9be49871f4fb989051bbd6
oon arfiandwi <oon@oo.or.id>
2014-12-21 01:55:42 +0700

    initial commit of project dilingkari (circled) for Google+.

---
+++ README.md
@@ -0,0 +1,14 @@
+## Project dilingkari (circled) for Google+
+
+http://dilingkari.appspot.com
+
+http://di.lingkari.com
+
+Open Terminal
+$ pip install -r requirements.txt -t lib/
+
+TODO:
+
+1. we don't need jinja2 and markupsafe because already available on App Engine, so you can delete it.
+
+1. we don't need \*.egg-info (to be uploaded to App Engine), so you can delete it.
---
+++ dilingkari.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
+
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+#if __name__ == '__main__':
+#     app.run()
